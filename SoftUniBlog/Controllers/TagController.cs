85b50f6637b1bf5612542ec4eca24b0ecce0a80c
DDimitrova90 <ddimitrova90@gmail.com>
2016-12-12 14:56:51 +0200

    updated Views/Tag/List.cshtml updated Controllers/TagController.cs

--- SoftUniBlog/Controllers/TagController.cs
+++ SoftUniBlog/Controllers/TagController.cs
@@ -6,13 +6,14 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace SoftUniBlog.Controllers
 {
     public class TagController : Controller
     {
         // GET: Tag
-        public ActionResult List(int? id)
+        public ActionResult List(int? id, int? page)
         {
             if (id == null)
             {
@@ -26,11 +27,13 @@ namespace SoftUniBlog.Controllers
                     .Include(t => t.Articles.Select(a => a.Tags))
                     .Include(t => t.Articles.Select(a => a.Author))
                     .FirstOrDefault(t => t.Id == id)
-                    .Articles
-                    .ToList();
+                    .Articles;
+
+                int pageSize = 3;
+                int pageNumber = (page ?? 1);
 
                 // Return the view
-                return View(articles);
+                return View(articles.ToPagedList(pageNumber, pageSize));
             }
         }
     }
